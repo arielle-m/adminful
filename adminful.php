6f33862a9a6309ee7dc4efb67c2f567f28c3b1d4
arielle-m <ariem.marii@gmail.com>
2025-02-17 22:06:10 -0800

    set up plugin file

---
+++ adminful.php
@@ -0,0 +1,15 @@
+<?php
+/*
+* Plugin Name:       Adminful
+* Plugin URI:        https://ariellemarin.com/
+* Description:       Customize your WordPress dashboard with White label, Dashboard Panels, Media Folders, Admin Menu Editor, Login Customizer, and much more!
+* Version:           1.0.0
+* Requires at least: 6.7.2
+* Requires PHP:      8.1.0
+* Author:            Arielle Marin
+* Author URI:        https://ariellemarin.com/
+* License:           GPL v2 or later
+* License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+* Update URI:        false
+*/
+?>
